fb6d7f8b59d163b21f0774ffae62e978dd3b8679
indrajityadav <67547613+indrajityadav@users.noreply.github.com>
2023-07-14 11:22:39 +0530

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Sunil");
+		System.out.println("Indrajit Yadav");
+		DemoApplication demo = new DemoApplication();
+		if(demo == null){
+			System.out.println("Object is null")
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
