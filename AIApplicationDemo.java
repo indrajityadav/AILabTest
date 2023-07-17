fd43cc7a1c1c465e3d03f0049e0911c3218d1da8
indrajityadav <67547613+indrajityadav@users.noreply.github.com>
2023-07-17 15:07:03 +0530

    Add files via upload

---
+++ AIApplicationDemo.java
@@ -0,0 +1,16 @@
+package com.ai.spring.app.aiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIApplicationDemo {
+
+	public static void main(String[] args) {
+		System.out.println("This application is running using tabnine aiapp.");
+		String str = null;
+		System.out.println(str);
+		SpringApplication.run(AIApplicationDemo.class, args);
+	}
+
+}
